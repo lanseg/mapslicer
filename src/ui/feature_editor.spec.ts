4db767ad9882dfbbc0dd38facc41575453ec5baa
Andrey Rusakov <andrey.rusakov@camptocamp.com>
2024-12-22 15:06:36 +0100

    Basic feature editor

--- src/ui/feature_editor.spec.ts
+++ src/ui/feature_editor.spec.ts
@@ -1,4 +1,4 @@
-import { describe, expect, test } from 'vitest'
+import { beforeEach, describe, expect, test } from 'vitest'
 
 import { Feature } from 'ol';
 import { Geometry } from 'ol/geom';
@@ -6,7 +6,36 @@ import { Geometry } from 'ol/geom';
 import { FeatureEditor } from './feature_editor';
 
 describe('FeatureEditorTest', () => {
-    test('can create', () => {
-        expect(new FeatureEditor(new Feature<Geometry>(), document.createElement("div"))).toBeTruthy();
+
+    let config: { parent: HTMLElement, feature: Feature<Geometry>, editor: FeatureEditor };
+
+    beforeEach(() => {
+        const parent = document.createElement("div");
+        const feature = new Feature<Geometry>();
+        config = {
+            parent,
+            feature,
+            editor: new FeatureEditor(feature, parent),
+        }
+        config.editor.render();
     });
+
+    function adder(): HTMLElement {
+        return  <HTMLElement> config.parent.querySelector('.add_property')!;
+    }
+
+    test('can create', () => {
+        expect(config.editor).toBeTruthy();
+        expect(config.parent.querySelector('.add_property')!.getAttribute('class')).toBeTruthy();
+    });
+
+    test('add features', () => {
+        adder().click();
+        adder().click();
+        adder().click();
+        expect(config.feature.getProperties()).toStrictEqual({
+            "Property 0": "", "Property 1": "", "Property 2": "",
+        });
+    });
+
 })
--- src/ui/feature_editor.ts
+++ src/ui/feature_editor.ts
@@ -3,26 +3,80 @@ import { Geometry } from 'ol/geom';
 
 const excludeFields = ['geometry'];
 
+function newCell(value: string, onchange: (now: string) => void): HTMLElement {
+    const input = document.createElement('input');
+    input.value = value;
+    input.addEventListener('blur', () => {
+        onchange(input.value);
+    });
+    input.addEventListener('keyup', (e) => {
+        if (e.code === 'Enter') {
+            onchange(input.value);
+        }
+    });
+
+    const cell = document.createElement('div');
+    cell.classList.add('cell');
+    cell.appendChild(input);
+    return cell;
+}
+
 export class FeatureEditor {
 
     constructor(private readonly feature: Feature<Geometry>, private readonly parent: HTMLElement) {
+    }
+
+    setProperty(key: string, value: string) {
+        this.feature.set(key, value);
+        this.render();
+    }
+
+    renderRow(key: string, value: string): Node {
+        const keyCell = newCell(key, (newKey) => {
+            this.feature.unset(key);
+            const newProperty: { [x: string]: string } = {};
+            newProperty[newKey] = value;
+            this.feature.setProperties(newProperty);
+            key = newKey;
+        });
+
+        const valueCell = newCell(value, (newValue) => {
+            const newProperty: { [x: string]: string } = {};
+            newProperty[key] = newValue;
+            this.feature.setProperties(newProperty);
+            value = newValue;
+        });
+
+        const result = document.createDocumentFragment();
+        result.append(keyCell, valueCell);
+        return result;
     }
 
     render() {
         const content = document.createElement('div');
         content.setAttribute('class', 'properties');
         content.innerHTML += '<div class="header">Feature</div>';
-        const props = this.feature.getProperties();
-        for (const [k, v] of Object.entries(props)) {
-            if (k in excludeFields) {
+        const props = Object.entries(this.feature.getProperties());
+        for (const [k, v] of props) {
+            if (excludeFields.indexOf(k) !== -1) {
                 continue;
             }
-            content.innerHTML += `
-              <div class="key">${k}</div>
-              <div class="value">${v}</div>
-            `;
-
+            content.appendChild(this.renderRow(k, v));
         }
+
+        const addRowControl = document.createElement('div');
+        addRowControl.setAttribute('class', 'add_property');
+        addRowControl.innerHTML = 'Add property';
+
+        const footer = document.createElement('div');
+        footer.setAttribute('class', 'footer');
+        footer.appendChild(addRowControl);
+        content.appendChild(footer);
+
+        addRowControl.addEventListener('click', () => {
+            this.setProperty(`Property ${props.length}`, '');
+        })
+
         this.parent.replaceChildren(content);
     }
 }
